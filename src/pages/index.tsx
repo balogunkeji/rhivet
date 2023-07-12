3c2a0ecd87153ff0638081444e22ca931675c78c
keji <tomitoyo.93@gmail.com>
2023-07-12 21:58:10 +0100

    commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,23 @@
-import Head from 'next/head'
-import React from 'react'
-import { HomeStyle } from '@/styles/homePage'
-import RhButton from '@/component/button'
-import { Illustration,Frame2, Linked, Airbnb, FedEx, Google, Hubspot, Microsoft, Walmart, Instagram, Dribble, Twitter, Youtube } from '../../public/image'
-import Image from 'next/image'
-import { offer } from '@/utiliities'
-import { PlaceHolderCards } from '@/component'
-import { useRouter } from 'next/router'
-
-
+import Head from "next/head";
+import React from "react";
+import { HomeStyle } from "@/styles/homePage";
+import RhButton from "@/component/button";
+import {
+  Illustration,
+  Frame2,
+  Linked,
+  Airbnb,
+  FedEx,
+  Google,
+  Hubspot,
+  Microsoft,
+  Walmart,
+  Twitter,
+} from "../../public/image";
+import Image from "next/image";
+import { offer } from "@/utiliities";
+import { PlaceHolderCards } from "@/component";
+import { useRouter } from "next/router";
 
 export default function Home() {
   const router = useRouter();
@@ -21,33 +30,37 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <HomeStyle>
-        <section className='hero'>
-          
-            <div className='hero__text'>
-              <div>
+        <section className="hero">
+          <div className="hero__text">
+            <div>
               <h4>Welcome to Rhivet</h4>
               <h1>Your Reliable Partner in Cybersecurity Excellence</h1>
               <div className="hero_imgs wite">
-           <div className='imgs'>
-            <Image src={Airbnb} alt='img'/>
-            <Image src={Hubspot} alt='img'/>
-            <Image src={Google} alt='img'/>
-            <Image src={Microsoft} alt='img'/>
-            <Image src={Walmart} alt='img'/>
-            <Image src={FedEx} alt='img'/>
-            </div>
-           </div>
+                <div className="imgs">
+                  <Image src={Airbnb} alt="img" />
+                  <Image src={Hubspot} alt="img" />
+                  <Image src={Google} alt="img" />
+                  <Image src={Microsoft} alt="img" />
+                  <Image src={Walmart} alt="img" />
+                  <Image src={FedEx} alt="img" />
+                </div>
               </div>
-              <RhButton text='Request Free Consultation' height='45px' width='250px' onClick={() => router.push('/contact-us')}/>
             </div>
-            <div className='hero__img'>
+            <RhButton
+              text="Request Free Consultation"
+              height="45px"
+              width="250px"
+              onClick={() => router.push("/contact-us")}
+            />
+          </div>
+          <div className="hero__img">
             {/* <Image src={Instagram} alt='img'/> */}
             {/* <Image src={Dribble} alt='img'/> */}
-            <Image src={Twitter} alt='img'/>
+            <Image src={Twitter} alt="img" />
             {/* <Image src={Youtube} alt='img'/> */}
-            <Image src={Linked} alt='img'/>
-            </div>
-           {/* <div className="hero_imgs">
+            <Image src={Linked} alt="img" />
+          </div>
+          {/* <div className="hero_imgs">
            <div className='imgs'>
             <Image src={Airbnb} alt='img'/>
             <Image src={Hubspot} alt='img'/>
@@ -58,39 +71,65 @@ export default function Home() {
             </div>
            </div> */}
         </section>
-        <section className='offer'>
+        <section className="offer">
           <div className="offerText">
-            <h3>We offer professional <br/> cyber security services</h3>
-            <p>Lorem ipsum dolor sit amet consectetur. Et ornare posuere quisque <br/> morbi egestas convallis. Adipiscing non enim aliquet penatibus platea </p>
+            <h3>
+              We offer professional <br /> cyber security services
+            </h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Et ornare posuere quisque{" "}
+              <br /> morbi egestas convallis. Adipiscing non enim aliquet
+              penatibus platea{" "}
+            </p>
           </div>
-            <div className='cards'>
-              {offer.map((item,index) => (
-                <PlaceHolderCards key={index} header={item.title} img={item.img} text={item.text}/>
-              ))}
-            </div>
+          <div className="cards">
+            {offer.map((item, index) => (
+              <PlaceHolderCards
+                key={index}
+                header={item.title}
+                img={item.img}
+                text={item.text}
+              />
+            ))}
+          </div>
         </section>
-        <section className='ways'>
-          <div className='ways__text'>
-            <h3>Ways to get rid of the <br/> growing cyber threat</h3>
-            <p>Lorem ipsum dolor sit amet consectetur. Et ornare posuere quisque morbi egestas convallis. Adipiscing non enim aliquet penatibus platea enim.Lorem ipsum dolor sit amet consectetur. Et ornare posuere quisque morbi egestas convallis. Adipiscing non enim aliquet penatibus platea enim.</p>
+        <section className="ways">
+          <div className="ways__text">
+            <h3>
+              Ways to get rid of the <br /> growing cyber threat
+            </h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Et ornare posuere quisque
+              morbi egestas convallis. Adipiscing non enim aliquet penatibus
+              platea enim.Lorem ipsum dolor sit amet consectetur. Et ornare
+              posuere quisque morbi egestas convallis. Adipiscing non enim
+              aliquet penatibus platea enim.
+            </p>
             {/* <RhButton text='Get Started' width='162px' height='45px'/> */}
           </div>
-          <div className='illustration'>
-            <Image src={Illustration} alt='image'/>
+          <div className="illustration">
+            <Image src={Illustration} alt="image" />
           </div>
         </section>
-        <section className='enhance'>
-            <div className="enhance__img">
-              <Image src={Frame2} alt='img'/>
-            </div>
-            <div className="enhanceText">
-              <h3>Enhancing Data Security for a Financial Institution: A Case Study</h3>
-              <p>This case study showcases the successful implementation of customized cybersecurity solutions for a prominent financial institution. By addressing their specific data security challenges and deploying robust measures, we ensured the protection of sensitive customer information and reduced the risk of breaches.</p>
-            </div>
-            {/* <RhButton text='Read More' width='120px' height='45px'/> */}
+        <section className="enhance">
+          <div className="enhance__img">
+            <Image src={Frame2} alt="img" />
+          </div>
+          <div className="enhanceText">
+            <h3>
+              Enhancing Data Security for a Financial Institution: A Case Study
+            </h3>
+            <p>
+              This case study showcases the successful implementation of
+              customized cybersecurity solutions for a prominent financial
+              institution. By addressing their specific data security challenges
+              and deploying robust measures, we ensured the protection of
+              sensitive customer information and reduced the risk of breaches.
+            </p>
+          </div>
+          {/* <RhButton text='Read More' width='120px' height='45px'/> */}
         </section>
-        
       </HomeStyle>
     </>
-  )
+  );
 }
--- src/styles/homePage.ts
+++ src/styles/homePage.ts
@@ -1,109 +1,105 @@
 import { styled } from "styled-components";
 
 export const HomeStyle = styled.main`
+  width: 100%;
+  max-width: 100%;
+  height: 100%;
+  padding-bottom: 200px;
 
+  .a {
+    color: white;
+    @media (max-width: 813px) {
+      color: #7c7c7c;
+    }
+  }
+  .hero {
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    justify-content: center;
     width: 100%;
-    max-width: 100%;
-    height: 100%;
-    padding-bottom: 200px;
+    padding: 100px 6%;
 
-    .a{
-        color: white;
-        @media(max-width: 813px){
-          color: #7c7c7c;
+    @media (min-width: 721px) {
+      justify-content: space-between;
+      padding: 100px 0;
+    }
+    .wite {
+      overflow: hidden;
+
+      img {
+        transform: translateX(100%);
+
+        animation: my-animation 15s linear infinite;
+      }
+
+      @keyframes my-animation {
+        from {
+          transform: translateX(100%);
         }
-      } 
-    .hero{
+        to {
+          transform: translateX(-100%);
+        }
+      }
+    }
+    .hero__img {
       display: flex;
-      align-items: center;
-      flex-wrap: wrap;
-      justify-content: center;
-      width: 100%;
-      padding: 100px 6%;
-
-      @media(min-width: 721px){
-        justify-content: space-between;
-        padding: 100px 0;
+      flex-direction: column;
+      padding-right: 6%;
+      gap: 19px;
+      justify-content: flex-start;
+      align-items: flex-start;
+      img:hover {
+        padding: 10px 0;
+        transition: padding 0.5s ease-in-out;
       }
-      .wite{
-        overflow: hidden;
-        
-        img{
-   
-  transform: translateX(100%);
-  
-  
-  animation: my-animation 15s linear infinite; 
-        }
-       
-
-@keyframes my-animation {
-  from {
-    transform: translateX(100%);
-  }
-  to {
-    transform: translateX(-100%);
-  }
-  }
-}
-      .hero__img{
-          display: flex;
-          flex-direction: column;
-          padding-right: 6%; 
-          gap: 19px;  
-          justify-content: flex-start; 
-          align-items: flex-start;
-					img:hover{
-        padding:  10px 0 ;
-        transition: padding .5s ease-in-out;
+      @media (max-width: 720px) {
+        display: none;
       }
-        @media(max-width: 720px){
-          display: none;
-        }
-      }
-      .hero__text{
+    }
+    .hero__text {
       max-width: 100%;
       display: flex;
       align-items: center;
       justify-content: center;
       flex-direction: column;
       gap: 65px;
-      
-      h4{
+
+      h4 {
         color: #f1f1f1;
         font-size: 20px;
         font-family: "Merriweather";
         font-weight: 400;
       }
-        p,h1,h4{
-          text-align: center;
-        }
-        @media(min-width: 721px){
-          justify-content: flex-start;
-          align-items: flex-start;
-          padding-left: 2%;
-          width: 55vw;
+      p,
+      h1,
+      h4 {
+        text-align: center;
+      }
+      @media (min-width: 721px) {
+        justify-content: flex-start;
+        align-items: flex-start;
+        padding-left: 2%;
+        width: 55vw;
 
-      h1,h4{
+        h1,
+        h4 {
           text-align: left;
         }
-        
       }
-      @media(min-width: 1200px){
+      @media (min-width: 1200px) {
         padding-top: 150px;
         padding-bottom: 100px;
         padding-left: 10%;
 
-        h1{
+        h1 {
           line-height: 1.2;
           width: 50vw;
-					margin: 20px 0;
-
+          margin: 20px 0;
         }
-      
       }
-      }
-    .hero_imgs{
+    }
+    .hero_imgs {
       width: 100%;
       display: flex;
       align-items: center;
@@ -111,146 +107,139 @@ export const HomeStyle = styled.main`
       margin-top: 20px;
       flex-direction: column;
       background: #fff;
-      
-      .imgs{
-      width: 100%;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      gap: 50px;
-      padding: 20px 6%;
-      overflow-x: scroll;
-  -ms-overflow-style: none;
-    scrollbar-width: none;
-    overflow-y: hidden;
 
-      img{
-
-        height: 25px;
-        object-fit: contain;
-      }
-    }
-    }
-
-    }
-    .offer{
-      display: flex;
-      flex-wrap: wrap;
-      justify-content: space-between;
-      max-width: 100%;
-      padding: 40px 6%;
-      align-items: center;
-      flex-direction: column;
-      gap: 32px;
-      h3{
-        text-align: center;
-      }
-      .cards{
-        display: grid;
-        gap: 10px;
-        grid-template-columns: repeat(auto-fill, minmax(350px, 3fr));
-        place-content: center;
-        justify-content: center;
-        align-items: center;
-        margin: auto;   
-        width: 80vw;  
-        
-        @media(max-width: 1024px){
-          grid-template-columns: repeat(auto-fill, minmax(320px, 3fr));
-          width: 100%;
-      }
-      }
-    }
-    
-    .ways{
-      display: flex;
-      flex-wrap: wrap;
-      justify-content: flex-start;
-      max-width: 100%;
-      padding: 30px 6%;
-      align-items: center;
-      gap: 32px;
-
-      @media(min-width: 1330px){
-        padding: 30px 8% 30px 12%;
-        gap: 170px;
-      }
-
-      .ways__text{
+      .imgs {
+        width: 100%;
         display: flex;
-        gap: 32px;
-        flex-direction: column;
-        @media(min-width: 800px){
-          width: 50%;
-        }
-        
-      }
-      .illustration{
-        margin: auto;
-        @media(min-width: 920px){
-          margin: 0;
-        }
-        img{
+        align-items: center;
+        justify-content: center;
+        gap: 50px;
+        padding: 20px 6%;
+        overflow-x: scroll;
+        -ms-overflow-style: none;
+        scrollbar-width: none;
+        overflow-y: hidden;
+
+        img {
+          height: 25px;
           object-fit: contain;
+        }
+      }
+    }
+  }
+  .offer {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    max-width: 100%;
+    padding: 40px 6%;
+    align-items: center;
+    flex-direction: column;
+    gap: 32px;
+    h3 {
+      text-align: center;
+    }
+    .cards {
+      display: grid;
+      gap: 10px;
+      grid-template-columns: repeat(auto-fill, minmax(350px, 3fr));
+      place-content: center;
+      justify-content: center;
+      align-items: center;
+      margin: auto;
+      width: 80vw;
+
+      @media (max-width: 1024px) {
+        grid-template-columns: repeat(auto-fill, minmax(320px, 3fr));
+        width: 100%;
+      }
+    }
+  }
+
+  .ways {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+    max-width: 100%;
+    padding: 30px 6%;
+    align-items: center;
+    gap: 32px;
+
+    @media (min-width: 1330px) {
+      padding: 30px 8% 30px 12%;
+      gap: 170px;
+    }
+
+    .ways__text {
+      display: flex;
+      gap: 32px;
+      flex-direction: column;
+      @media (min-width: 800px) {
+        width: 50%;
+      }
+    }
+    .illustration {
+      margin: auto;
+      @media (min-width: 920px) {
+        margin: 0;
+      }
+      img {
+        object-fit: contain;
         /* max-width: 300px; */
         height: 325px;
         filter: grayscale(1);
-        @media(min-width: 923px){
+        @media (min-width: 923px) {
           height: 480px;
-        }
         }
       }
     }
+  }
 
-    .enhance{
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: flex-start;
-        max-width: 100%;
-        padding: 30px 6%;
-        align-items: center;
-        gap: 32px;
-        h3{
-          font-weight: 400;
-          margin-bottom: 10px;
-          color: #fff;
-        }
-        p{ 
-          font-size: 14px;
-          color: #fff;
-
-        }
-        @media(min-width: 801px){
-          position: relative;
-
-          .enhance__img{
-            img{
-              width: 100%;
-            }
-            /* position: relative; */
-          }
-          .enhanceText{
-            position: absolute;
-            gap: 20.94px;
-            width: 40vw;
-            bottom: 0;
-            right: 60px;
-            background: #000;
-            height: fit-content;
-            padding: 20px 2% 40px 2%;
-            margin-bottom: 20px;
-          }
-          button{
-            margin-left: 80vw;
-            z-index: 1;
-
-          }
-        }
-       
+  .enhance {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+    max-width: 100%;
+    padding: 30px 6%;
+    align-items: center;
+    gap: 32px;
+    h3 {
+      font-weight: 400;
+      margin-bottom: 10px;
+      color: #fff;
     }
-
-    .contact{
-      padding: 0 6%;
+    p {
+      font-size: 14px;
+      color: #fff;
     }
+    @media (min-width: 801px) {
+      position: relative;
 
+      .enhance__img {
+        img {
+          width: 100%;
+        }
+        /* position: relative; */
+      }
+      .enhanceText {
+        position: absolute;
+        gap: 20.94px;
+        width: 40vw;
+        bottom: 0;
+        right: 60px;
+        background: #000;
+        height: fit-content;
+        padding: 20px 2% 40px 2%;
+        margin-bottom: 20px;
+      }
+      button {
+        margin-left: 80vw;
+        z-index: 1;
+      }
+    }
+  }
+
+  .contact {
+    padding: 0 6%;
+  }
 `;
